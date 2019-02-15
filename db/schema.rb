a6a1d6decc49f7688b7d446184e41cc959f8549c
mao <maohir9u1@gmail.com>
2019-02-15 19:14:03 +0900

    add_post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_084609) do
+ActiveRecord::Schema.define(version: 2019_02_15_100140) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
