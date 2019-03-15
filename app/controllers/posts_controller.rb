2fb3e14a52be0169f2aa389e4cc458aec35b7ae5
清水麻緒 <shimizumao@shimizumaonoMacBook-Pro.local>
2019-03-15 17:36:21 +0900

    home index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :authenticate
+    before_action :authenticate, only: [:new, :create]
 
     def new
         @post = Post.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root to: "pages#index"
+	root to: "posts#index"
 	resources :posts, only: [:index, :show, :new, :create] do
 		resources :likes, only: :create
 	end
