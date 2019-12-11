73e7592091e0784db541d77b9ba8c4fba926a7f4
清水麻緒 <shimizumao@shimizumaonoMacBook-Pro.local>
2019-12-12 00:05:37 +0900

    user_show view

---
+++ db/migrate/20191210082148_add_column_users.rb
@@ -0,0 +1,5 @@
+class AddColumnUsers < ActiveRecord::Migration[5.2]
+	def change
+		add_column :users, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_112434) do
+ActiveRecord::Schema.define(version: 2019_12_11_143851) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_02_28_112434) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
